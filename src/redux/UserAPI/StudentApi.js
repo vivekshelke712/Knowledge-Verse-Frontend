19e882b12ae1f5b247b1e8de624d04da6497a7f8
Vivek Shelke <88998663+vivekshelke712@users.noreply.github.com>
2025-03-10 22:46:29 +0530

    Update StudentApi.js

--- src/redux/UserAPI/StudentApi.js
+++ src/redux/UserAPI/StudentApi.js
@@ -1,1 +1,44 @@
 
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const ContactSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  tagTypes: ["tagName"],
+  endpoints: (builder) => {
+    return {
+      getUsers: builder.query({
+        query: () => {
+          return {
+            url: "/apiEndPoint",
+            method: "GET",
+          };
+        },
+        providesTags: ["tagName"],
+      }),
+      addUser: builder.mutation({
+        query: (userData) => {
+          return {
+            url: "/apiEndPoint",
+            method: "POST",
+            body: userData,
+          };
+        },
+        invalidatesTags: ["tagName"],
+      }),
+    };
+UpdateUser: builder.mutation({
+        query: (userData) => {
+          return {
+            url: "/apiEndPoint",
+            method: "POST",
+            body: userData,
+          };
+        },
+        invalidatesTags: ["tagName"],
+      }),
+    };
+  },
+});
+
+export const { useGetUsersQuery, useAddUserMutation } = ContactSlice;
